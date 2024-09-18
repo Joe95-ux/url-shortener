d5e9851a063c5db6ab81a0c38d300c70452fa12f
joe95-ux <ogorktabi@gmail.com>
2024-09-18 22:12:11 +0200

    add components

---
+++ components/url-list.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function UrlList() {
+  return (
+    <div>
+        <h2 className="text-2xl font-bold mb-2">
+           Recent Urls
+        </h2>
+
+    </div>
+  )
+}
