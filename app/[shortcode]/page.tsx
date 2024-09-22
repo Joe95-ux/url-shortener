976df72ac27fe1afeb29ffcf0160934274369c4f
joe95-ux <ogorktabi@gmail.com>
2024-09-22 22:51:52 +0200

    handle url redirects

---
+++ app/[shortcode]/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation";
+import prisma from "../../lib/db";
+
+interface RedirectPageProps {
+  params: { shortcode: string };
+}
+const RedirectPage = async ({ params }: RedirectPageProps) => {
+  const { shortcode } = params;
+
+  const url = await prisma.findUnique({
+    where: { shortCode: shortcode },
+  });
+  if (!url) {
+    return <div> 404 - URL not found</div>;
+  }
+  
+  await prisma.url.update({
+    where:{id: url.id},
+    data: {visits: {increment: 1}}
+  })
+  redirect(url.originalUrl);
+};
+
+export default RedirectPage;
