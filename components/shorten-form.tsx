414f1a67b5dba4ed0d4f69e527ce6f23c5bbd998
joe95-ux <ogorktabi@gmail.com>
2024-09-26 20:58:43 +0200

    properly handle form submissions with react-hook-form and zod

--- components/shorten-form.tsx
+++ components/shorten-form.tsx
@@ -3,60 +3,58 @@
 import React from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { useState } from "react";
 import { toast } from "sonner";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import {urlFormSchema, UrlFormSchemaType} from "@/utils/form";
+import { urlFormSchema, UrlFormSchemaType } from "@/utils/form";
 
 interface ShortenFormProps {
   handleUrlShortened: () => void;
 }
 
-
 export default function ShortenForm({ handleUrlShortened }: ShortenFormProps) {
-  const [url, setUrl] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  
-  const form = useForm<UrlFormSchemaType>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset
+  } = useForm<UrlFormSchemaType>({
     resolver: zodResolver(urlFormSchema),
   });
-  
-  const handleSubmit = async (e: React.FormEvent, data: UserForm) => {
-    e.preventDefault();
-    setIsLoading(true);
+
+  const onSubmit = async (data: UrlFormSchemaType) => {
     try {
       const response = await fetch("/api/shorten", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ url }),
+        body: JSON.stringify(data), // Send form data directly
       });
+      if (!response.ok) {
+        throw new Error("Error shortening URL");
+      }
       await response.json();
-      setUrl("");
-      toast.success("Url shortened successfully");
+      reset(); // Reset the form after successful submission
+      toast.success("URL shortened successfully");
       handleUrlShortened();
     } catch (error) {
       console.error("Error shortening URL:", error);
       toast.error("Error shortening URL");
-    } finally {
-      setIsLoading(false);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mb-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="mb-4">
       <div className="space-y-4">
         <Input
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
+          {...register("url")}
           className="h-12"
           type="url"
           placeholder="Enter URL to shorten"
-          required
         />
-        <Button className="w-full p-2" type="submit" disabled={isLoading}>
-          {isLoading ? "shortening url..." : "Shorten Url"}
+        {errors.url && <p className="text-red-500">{errors.url.message}</p>} {/* Display validation error */}
+
+        <Button className="w-full p-2" type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Shortening URL..." : "Shorten URL"}
         </Button>
       </div>
     </form>
